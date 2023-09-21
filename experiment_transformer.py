76154a31d3dab670c925225c90e2cd40b74c5c24
abthuy <arthur.thuy@ugent.be>
2023-09-21 18:08:15 +0200

    initialize + run transformer

---
+++ experiment_transformer.py
@@ -0,0 +1,59 @@
+import yaml
+import logging
+
+from qdet_utils.experiment import TransformerExperiment
+from qdet_utils.constants import TF_Q_ALL
+
+# set logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main(experiment_config):
+    model_type = experiment_config["model_type"]
+    dataset_name = experiment_config["dataset_name"]
+
+    random_seed = (
+        experiment_config["random_seed"]
+        if experiment_config["random_seed"] != "None"
+        else None
+    )
+
+    if model_type == "transformer":
+        model_name = (
+            experiment_config["model_name"]
+            if experiment_config["model_name"] is not None
+            else "transformer"
+        )
+        experiment = TransformerExperiment(
+            dataset_name=dataset_name, random_seed=random_seed
+        )
+        logger.info("Starting dataset loading")
+        experiment.get_dataset(TF_Q_ALL)
+        logger.info("Starting model initialization")
+        experiment.init_model(
+            experiment_config["pretrained_model"],
+            model_name,
+            experiment_config["max_length"],
+            experiment_config["pretrained_tokenizer"],
+        )
+        logger.info("Starting training")
+        experiment.train(
+            experiment_config["epochs"],
+            experiment_config["batch_size"],
+            experiment_config["early_stopping_patience"],
+            experiment_config["learning_rate"],
+            experiment_config["weight_decay"],
+        )
+        logger.info("Starting prediction")
+        experiment.predict(experiment_config["eval_batch_size"])
+        logger.info("Starting evaluation")
+        experiment.evaluate()
+        return
+
+    print("Unknown model_type!")
+
+
+if __name__ == "__main__":
+    config = yaml.safe_load(open("experiment_config.yaml", mode="r", encoding="utf-8"))
+    main(config)
