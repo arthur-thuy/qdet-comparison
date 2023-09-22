9dce0520eab1dd07c8df0f3faceeca8e4f0d5e36
abthuy <arthur.thuy@ugent.be>
2023-09-22 16:01:23 +0200

    run majority and random

--- qdet_utils/experiment/_random.py
+++ qdet_utils/experiment/_random.py
@@ -5,16 +5,16 @@ import pickle
 import random
 
 from qdet_utils.experiment import BaseExperiment
-from qdet_utils.constants import Q_ID, OUTPUT_DIR, DATA_DIR
+from qdet_utils.constants import Q_ID, OUTPUT_DIR, DATA_DIR, TF_DIFFICULTY
 
 
 class RandomExperiment(BaseExperiment):
     def __init__(
-            self,
-            dataset_name: str,
-            data_dir: str = DATA_DIR,
-            output_root_dir: str = OUTPUT_DIR,
-            random_seed: Optional[int] = None,
+        self,
+        dataset_name: str,
+        data_dir: str = DATA_DIR,
+        output_root_dir: str = OUTPUT_DIR,
+        random_seed: Optional[int] = None,
     ):
         super().__init__(dataset_name, data_dir, output_root_dir, random_seed)
         self.true_difficulties = None
@@ -22,14 +22,28 @@ class RandomExperiment(BaseExperiment):
         self.max_diff = None
 
     def get_dataset(self, *args, **kwargs):
-        # TODO I should redo this in order not to use the t2p dataframe
-        self.df_train = pd.read_csv(os.path.join(self.data_dir, f't2p_{self.dataset_name}_train.csv'), dtype={Q_ID: str})
-        self.df_test = pd.read_csv(os.path.join(self.data_dir, f't2p_{self.dataset_name}_test.csv'), dtype={Q_ID: str})
-        self.y_true_train = pickle.load(open(os.path.join(self.data_dir, f'y_true_train_{self.dataset_name}.p'), 'rb'))
-        self.y_true_dev = pickle.load(open(os.path.join(self.data_dir, f'y_true_dev_{self.dataset_name}.p'), 'rb'))
-        self.y_true_test = pickle.load(open(os.path.join(self.data_dir, f'y_true_test_{self.dataset_name}.p'), 'rb'))
+        self.df_train = pd.read_csv(
+            os.path.join(
+                self.data_dir, f"tf_{self.dataset_name}_text_difficulty_train.csv"
+            ),
+            dtype={Q_ID: str},
+        )
+        self.df_test = pd.read_csv(
+            os.path.join(
+                self.data_dir, f"tf_{self.dataset_name}_text_difficulty_test.csv"
+            ),
+            dtype={Q_ID: str},
+        )
+        self.y_true_train = self.df_train[TF_DIFFICULTY].values
+        self.y_true_test = self.df_test[TF_DIFFICULTY].values
 
-    def init_model(self, pretrained_model: Optional = None, model_name: str = 'model', *args, **kwargs):
+    def init_model(
+        self,
+        pretrained_model: Optional[str] = None,
+        model_name: str = "model",
+        *args,
+        **kwargs,
+    ):
         self.model_name = model_name
 
     def train(self, *args, **kwargs):
@@ -41,11 +55,38 @@ class RandomExperiment(BaseExperiment):
 
     def predict(self, save_predictions: bool = True):
         if self.discrete_regression:
-            self.y_pred_train = [random.choice(self.true_difficulties) for _ in range(len(self.df_train))]
-            self.y_pred_test = [random.choice(self.true_difficulties) for _ in range(len(self.df_test))]
+            self.y_pred_train = [
+                random.choice(self.true_difficulties) for _ in range(len(self.df_train))
+            ]
+            self.y_pred_test = [
+                random.choice(self.true_difficulties) for _ in range(len(self.df_test))
+            ]
         else:
-            self.y_pred_train = [random.random() * (self.max_diff - self.min_diff) + self.min_diff for _ in range(len(self.df_train))]
-            self.y_pred_test = [random.random() * (self.max_diff - self.min_diff) + self.min_diff for _ in range(len(self.df_test))]
+            self.y_pred_train = [
+                random.random() * (self.max_diff - self.min_diff) + self.min_diff
+                for _ in range(len(self.df_train))
+            ]
+            self.y_pred_test = [
+                random.random() * (self.max_diff - self.min_diff) + self.min_diff
+                for _ in range(len(self.df_test))
+            ]
         if save_predictions:
-            pickle.dump(self.y_pred_test, open(os.path.join(self.output_dir, 'predictions_test_' + self.model_name + '.p'), 'wb'))
-            pickle.dump(self.y_pred_train, open(os.path.join(self.output_dir, 'predictions_train_' + self.model_name + '.p'), 'wb'))
+            os.makedirs(self.output_dir, exist_ok=True)
+            pickle.dump(
+                self.y_pred_test,
+                open(
+                    os.path.join(
+                        self.output_dir, "predictions_test_" + self.model_name + ".p"
+                    ),
+                    "wb",
+                ),
+            )
+            pickle.dump(
+                self.y_pred_train,
+                open(
+                    os.path.join(
+                        self.output_dir, "predictions_train_" + self.model_name + ".p"
+                    ),
+                    "wb",
+                ),
+            )
